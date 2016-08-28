235af7d9375f435ec598209a930e1f8e81c53654
naresh89 <naparim@NAPARIM-IN.idc.oracle.com>
2016-08-28 22:43:25 +0530

    Update crawler, README and pom files

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # web-crawler
-This reposiitory contains code snippets for Web crawler
+This repository contains code implementation for simple Web Crawler demonstration as part of the assignment.
+
+Jsoup java library is used for this purpose.Following Jsoup dependency snippet is added to pom.xml.
+  
+   ```<dependency>
+        <groupId>org.jsoup</groupId>
+        <artifactId>jsoup</artifactId>
+        <version>1.7.2</version>
+      </dependency>
+   ```
+
+
+##Guidelines to run this web-crawler standalone app:
+
+ Clone the repository and follow the below simple steps to build this maven project and run
+
+     `$mvn package`
+       
+     `$mvn install assembly:assembly`
+
+ Now you will have the jar with dependencies in your target folder.
+
+ Run the below java command by giving any URL as input argument
+
+     `$java -cp target/web-crawler-1.0-SNAPSHOT-jar-with-dependencies.jar com.webcrawler.core.App <URL>`
+
+     Example:
+
+     `$java -cp target/web-crawler-1.0-SNAPSHOT-jar-with-dependencies.jar com.webcrawler.core.App http://wiprodigital.com/`
+
--- src/main/java/com/webcrawler/core/App.java
+++ src/main/java/com/webcrawler/core/App.java
@@ -1,13 +1,56 @@
 package com.webcrawler.core;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
 /**
- * Hello world!
+ * This is simple Web Crawler for the given use case given by Wipro Team as part of assesment.
  *
+ * Input URL : http://wiprodigital.com/
+ * 
+ * Output : Links to other pages, Links to statis pages like images
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        System.out.println("\n Hello! Welcome to the web crawler demonstration as part of the Wipro assignment \n" );
+        
+        if(args.length==0 || args.length>1){
+        	System.err.println("Example Usage:: java -cp target/web-crawler-1.0-SNAPSHOT-jar-with-dependencies.jar com.webcrawler.core.App http://wiprodigital.com/");
+        	return;
+        }
+
+        crawlerController(args[0]);
+    }
+
+    public static void crawlerController(String URL) throws IOException {
+    	System.out.println("START of WebCrawler for the given URL: "+URL);
+    	Document doc = Jsoup.connect(URL).get();
+
+    	Elements links = doc.select("a[href]");
+        Elements media = doc.select("[src]");
+        Elements externalLinks = doc.select("link[href]");
+        
+        System.out.println("\n----List of links to other pages----\n");
+        for(Element e: links){
+        	System.out.println(e.attr("abs:href"));
+        }
+ 
+        System.out.println("\n----List of static images ----\n");
+        for(Element e: media){
+        	if(e.tagName().equals("img")){
+        	   System.out.println(e.attr("abs:src"));
+        	  }
+        }
+
+        System.out.println("\n----List of links to imports----\n");
+        for(Element e: externalLinks){
+        	System.out.println(e.attr("abs:href"));
+        }
     }
 }
